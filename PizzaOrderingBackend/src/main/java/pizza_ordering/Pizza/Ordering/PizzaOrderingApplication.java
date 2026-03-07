e561154624d8f0eb7edd5f6a0f79bad6ca27e859
Kaushik Shinde <shindekaushik88@gmail.com>
2026-03-07 15:06:45 +0530

    project setup

---
+++ PizzaOrderingBackend/src/main/java/pizza_ordering/Pizza/Ordering/PizzaOrderingApplication.java
@@ -0,0 +1,13 @@
+package pizza_ordering.Pizza.Ordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaOrderingApplication.class, args);
+	}
+
+}
